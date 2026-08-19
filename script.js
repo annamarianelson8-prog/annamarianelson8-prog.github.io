f6ad35859b3daa8fa65d39bdf9b531c6dcfe0e90
annamarianelson8-prog <annamarianelson8@gmail.com>
2026-08-18 21:40:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,39 @@ if (currentHour < 12) {
 }
 
 greetingElement.textContent = greeting + ", Anna Maria!";
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    const formMessage = document.getElementById("formMessage");
+
+    if (name === "") {
+        formMessage.textContent = "Please enter your name.";
+        return;
+    }
+
+    if (email === "") {
+        formMessage.textContent = "Please enter your email.";
+        return;
+    }
+
+    if (!email.includes("@")) {
+        formMessage.textContent = "Please enter a valid email.";
+        return;
+    }
+
+    if (message === "") {
+        formMessage.textContent = "Please enter your message.";
+        return;
+    }
+
+    formMessage.textContent = `Thank you, ${name}! Your message has been submitted.`;
+
+    contactForm.reset();
+});
